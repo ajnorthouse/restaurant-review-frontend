912d8cbb30fb8cb2ea81f5aa40c53f4dc5af9b59
Alexandre Northouse <Alex.J.Northouse@gmail.com>
2021-03-25 12:12:41 -0400

    Refactor components structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import MainPage from './components/Home-Page/MainPage';
-import NavBar from './components/Home-Page/NavBar';
-import Footer from './components/Home-Page/Footer';
+import NavBar from './components//NavBar';
+import Footer from './components/Footer';
 
 function App() {
   return (
