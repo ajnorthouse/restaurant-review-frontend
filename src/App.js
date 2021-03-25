9663238023f0e21e656c1ad9ef138b76b1befc4f
Alexandre Northouse <Alex.J.Northouse@gmail.com>
2021-03-25 12:02:33 -0400

    Setup main page skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import MainPage from './components/Home-Page/MainPage';
+import NavBar from './components/Home-Page/NavBar';
+import Footer from './components/Home-Page/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <MainPage />
+      <Footer />
     </div>
   );
 }
