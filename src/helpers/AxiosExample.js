6f5851e910f6e6f6f31be0b41496d9b43aea1abf
Alexandre Northouse <Alex.J.Northouse@gmail.com>
2021-03-30 16:32:38 -0400

    Final implementation of API calling class

--- src/helpers/AxiosExample.js
+++ src/helpers/AxiosExample.js
@@ -1,27 +1,93 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 const databaseUrl = "http://localhost:8080/api/";
+export const table = {
+  ADMIN: 'admin/',
+  USER: 'user/',
+  RESTAURANT: 'restaurant/',
+  REVIEW: 'review/'
+}
+export const getMethod = {
+  ALL:'',
+  BY_ID:'id/',
+  BY_NAME:'name/',
+  BY_USERNAME:'username/',
+  BY_RATING:'rating/',
+  BY_USERID:'userid/',
+  BY_RESTAURANTID:'restaurantid/'
+}
+const htmlRequest = {
+  POST:'create/',
+  GET:'',
+  PUT:'update/',
+  PATCH:'update/',
+  DELTE:'delete/'
+}
 
-//get (read) requests
-export const useFetch = (searchMethod, searchValue) => {
-  let param = 0;
+// const for the get requests
+export const useFetch = (getTable, method, searchValue) => {
+  let searchValueIsNum = (searchValue !== undefined && searchValue !== null && typeof(searchValue)==='number');
+  let searchValueIsStr = (searchValue !== undefined && searchValue !== null && typeof(searchValue)==='string');
+  let getUrl;
+  let continueGet = true;
 
-  if (searchValue !== undefined) param = searchValue;
 
-  const getAll = 'http://swapi.dev/api/people/';
-  const getById = 'http://swapi.dev/api/people/';
-  const search = 'http://swapi.dev/api/people/' + param;
-  let url = null;
-
-  switch (searchMethod) {
-    case 'getById': url = getById;
+  //switch-case statement on the table name (getTable)
+  switch(getTable) {
+    case table.RESTAURANT:
+      getUrl = databaseUrl + getTable + htmlRequest.GET;
       break;
-    case 'getAll': url = getAll;
+    case table.ADMIN:
+      getUrl = databaseUrl + getTable + htmlRequest.GET;
       break;
-    case 'search': url = search;
+    case table.REVIEW:
+      getUrl = databaseUrl + getTable + htmlRequest.GET;
       break;
-    default: url = null;
+    case table.USER:
+      getUrl = databaseUrl + getTable + htmlRequest.GET;
+      break;
+    default:
+      continueGet = false;
+      console.log("Bad Table Name");
   }
+  
+
+  //switch-case statement on the type of get (method)
+  switch(method) {
+    case getMethod.ALL:
+      getUrl = getUrl;
+      break;
+    case getMethod.BY_ID:
+      searchValue = (searchValueIsNum) ? searchValue : 0;
+      getUrl = getUrl + method + searchValue;
+      break;
+    case getMethod.BY_NAME:
+      searchValue = (searchValueIsStr) ? searchValue : 'NA';
+      getUrl = getUrl + method + searchValue;
+      break;
+    case getMethod.BY_USERNAME:
+      searchValue = (searchValueIsStr) ? searchValue : 'NA';
+      getUrl = getUrl + method + searchValue;
+      break;
+    case getMethod.BY_RATING:
+      searchValue = (searchValueIsNum) ? searchValue : 0.0;
+      getUrl = getUrl + method + searchValue;
+      break;
+    case getMethod.BY_USERID:
+      searchValue = (searchValueIsNum) ? searchValue : 0;
+      getUrl = getUrl + method + searchValue;
+      break;
+    case getMethod.BY_RESTAURANTID:
+      searchValue = (searchValueIsNum) ? searchValue : 0;
+      getUrl = getUrl + method + searchValue;
+      break;
+    default:
+      continueGet = false;
+      console.log("Bad Request Method");
+  }
+
+  //this stores the getUrl as url and keeps original functionality intact.
+  let url = (continueGet) ? getUrl : null;
 
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -47,51 +113,51 @@ export const useFetch = (searchMethod, searchValue) => {
 };
 
 //post (create) requests
-export function createEntry(table, entry) {
+export function createEntry(createTable, entry) {
   //switch-case statement on the table name
-  switch(table) {
-    case "restaurant":
-      return axios.post(databaseUrl + "/restaurant/", entry);
-    case "admin":
-      return axios.post(databaseUrl + "/admin/", entry);
-    case "review":
-      return axios.post(databaseUrl + "/review/", entry);
-    case "user":
-      return axios.post(databaseUrl + "/user/", entry);
+  switch(createTable) {
+    case table.RESTAURANT:
+      return axios.post(databaseUrl + table.RESTAURANT + htmlRequest.POST, entry);
+    case table.ADMIN:
+      return axios.post(databaseUrl + table.ADMIN + htmlRequest.POST, entry);
+    case table.REVIEW:
+      return axios.post(databaseUrl + table.REVIEW + htmlRequest.POST, entry);
+    case table.USER:
+      return axios.post(databaseUrl + table.USER + htmlRequest.POST, entry);
     default:
       console.log("Bad Request");
   }
 }
 
-//update requests
-export function updateEntry(table, entry) {
+//put (update) requests
+export function putEntry(putTable, entry) {
   //switch-case statement on the table name
-  switch(table) {
-    case "restaurant":
-      return axios.put(databaseUrl + "/restaurant/" + entry.id, entry);
-    case "admin":
-      return axios.put(databaseUrl + "/admin/" + entry.id, entry);
-    case "review":
-      return axios.put(databaseUrl + "/review/" + entry.id, entry);
-    case "user":
-      return axios.put(databaseUrl + "/user/" + entry.id, entry);
+  switch(putTable) {
+    case table.RESTAURANT:
+      return axios.put(databaseUrl + table.RESTAURANT + htmlRequest.PUT, entry);
+    case table.ADMIN:
+      return axios.put(databaseUrl + table.ADMIN + htmlRequest.PUT, entry);
+    case table.REVIEW:
+      return axios.put(databaseUrl + table.REVIEW + htmlRequest.PUT, entry);
+    case table.USER:
+      return axios.put(databaseUrl + table.USER + htmlRequest.PUT, entry);
     default:
       console.log("Bad Request");
   }
 }
 
 //delete requests
-export function deleteEntry(table, entryId) {
+export function deleteEntry(deleteTable, entryId) {
   //switch-case statement on the table name
-  switch(table) {
-    case "restaurant":
-      return axios.delete(databaseUrl + "/restaurant/" + entryId);
-    case "admin":
-      return axios.delete(databaseUrl + "/admin/" + entryId);
-    case "review":
-      return axios.delete(databaseUrl + "/review/" + entryId);
-    case "user":
-      return axios.delete(databaseUrl + "/user/" + entryId);
+  switch(deleteTable) {
+    case table.RESTAURANT:
+      return axios.put(databaseUrl + table.RESTAURANT + htmlRequest.DELETE + entryId);
+    case table.ADMIN:
+      return axios.put(databaseUrl + table.ADMIN + htmlRequest.DELETE + entryId);
+    case table.REVIEW:
+      return axios.put(databaseUrl + table.REVIEW + htmlRequest.DELETE + entryId);
+    case table.USER:
+      return axios.put(databaseUrl + table.USER + htmlRequest.DELETE + entryId);
     default:
       console.log("Bad Request");
   }
